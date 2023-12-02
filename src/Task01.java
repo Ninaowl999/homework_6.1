98723af85ec8848238efb052393c72e8c31d92a3
Ninaowl999 <128140719+Ninaowl999@users.noreply.github.com>
2023-12-03 01:47:17 +0400

    Commit add Task01.java

---
+++ src/Task01.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Task01 {
+    public static void main(String[] args) {
+        System.out.println("Задание №1");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите строку текста: ");
+        String text = scanner.nextLine();
+
+        System.out.print("Введите символ: ");
+        char symbol = scanner.nextLine().charAt(0);
+
+        ArrayList<Integer> indices = new ArrayList<Integer>();
+        int count = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == symbol) {
+                indices.add(i);
+                count++;
+            }
+        }
+        if (count == 0) {
+            System.out.println("Совпадений не найдено." + " Соответствующий текст: " + text);
+        } else {
+            System.out.println("Индексы совпадений: " + indices);
+            System.out.println("Количество совпадений: " + count);
+        }
+    }
+}
+
+
